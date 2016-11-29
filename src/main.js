602e2f7995eef0371525853d4b13fdfa2781dc60
Jeng <kinkongjeng@hotmail.com>
2016-11-30 00:33:36 +0700

    completed tutorial

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
 import Vue from 'vue'
+import '!style!css!semantic-ui-css/semantic.css'
+import '!script!jquery'
+import '!script!semantic-ui-css/semantic.js'
 import App from './App'
 
 /* eslint-disable no-new */
