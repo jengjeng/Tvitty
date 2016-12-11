6c36d33423f7c954da9c81ff79e1c7ad69789b34
jengjeng <kinkongjeng@gmail.com>
2016-12-11 14:24:45 +0700

    fix signin with google

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -7,3 +7,26 @@ firebase.initializeApp({
   storageBucket: 'tvitty-b69b1.appspot.com',
   messagingSenderId: '79608916460'
 })
+
+firebase.auth().onAuthStateChanged(user => {
+  console.log(user)
+  if (user) {
+    // // save the user's profile into the database so we can list users,
+    // // use them in Security and Firebase Rules, and show profiles
+    // firebase.child("users").child(authData.uid).set({
+    //   provider: authData.provider,
+    //   name: getName(authData)
+    // })
+  }
+})
+// find a suitable name based on the meta info given by each provider
+// function getName(authData) {
+//   switch(authData.provider) {
+//      case 'password':
+//        return authData.password.email.replace(/@.*/, '')
+//      case 'twitter':
+//        return authData.twitter.displayName
+//      case 'facebook':
+//        return authData.facebook.displayName
+//   }
+// }
--- src/services/user.js
+++ src/services/user.js
@@ -1,11 +1,13 @@
 import firebase from 'firebase'
 
+const db = firebase.database()
+
 export default {
   get currentUser () {
     return this.get()
   },
   get () {
-    return firebase.auth().currentUser
+    return setUserData(firebase.auth().currentUser)
   },
   save (obj) {
     for (let prop in obj) {
@@ -14,9 +16,34 @@ export default {
     return this.currentUser
   },
   signInWithGoogle () {
-    return firebase.auth().signInWithPopup(new firebase.auth.GoogleAuthProvider())
+    let _user
+    return new Promise((resolve, reject) => {
+      firebase.auth().signInWithPopup(new firebase.auth.GoogleAuthProvider()).then((user) => {
+        _user = user
+        return db.ref(`/users/${_user.uid}`).once('value')
+      }).then((snapshot) => {
+        if (snapshot.exists()) {
+          resolve()
+        } else {
+          db.ref(`/users/${_user.uid}`).set({
+            description: ''
+          }).then(() => {
+            resolve()
+          })
+        }
+      })
+    })
   },
   signOut () {
     return firebase.auth().signOut()
+  }
+}
+
+/* Functions */
+function setUserData (user, userVal) {
+  user = user || {}
+  for (let key in userVal) {
+    console.log(key, userVal[key])
+    user[key] = userVal[key]
   }
 }
