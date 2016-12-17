3167f580bea599aaeb1017d6afc0257070397602
Jeng <kinkongjeng@hotmail.com>
2016-12-17 22:43:08 +0700

    add vue-devtools config

--- src/main.js
+++ src/main.js
@@ -10,6 +10,8 @@ import router from './router'
 import store from './store'
 import App from './App'
 
+Vue.config.devtools = process.env.DEBUG
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
