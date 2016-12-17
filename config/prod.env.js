20bfd375de5120680a32d1d17024d7ce68411939
Jeng <kinkongjeng@hotmail.com>
2016-12-17 22:08:47 +0700

    done: server configs

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,3 +1,11 @@
 module.exports = {
-  NODE_ENV: '"production"'
+  NODE_ENV: '"production"',
+  DEBUG: "false",
+  FIREBASE: `{
+    apiKey: 'AIzaSyA7deC1Etp4mU6QFKuxCCmZ_vfYpgkCogc',
+    authDomain: 'tvitty-b69b1.firebaseapp.com',
+    databaseURL: 'https://tvitty-b69b1.firebaseio.com',
+    storageBucket: 'tvitty-b69b1.appspot.com',
+    messagingSenderId: '79608916460'
+  }`
 }
--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,9 +1,3 @@
 import firebase from 'firebase'
 
-export default firebase.initializeApp({
-  apiKey: 'AIzaSyA7deC1Etp4mU6QFKuxCCmZ_vfYpgkCogc',
-  authDomain: 'tvitty-b69b1.firebaseapp.com',
-  databaseURL: 'https://tvitty-b69b1.firebaseio.com',
-  storageBucket: 'tvitty-b69b1.appspot.com',
-  messagingSenderId: '79608916460'
-})
+export default firebase.initializeApp(process.env.FIREBASE)
